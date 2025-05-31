b02758d8de2da88931579612c3ea91027ec08dfb
israelluze <israelluze@gmail.com>
2025-05-30 22:31:16 -0300

    inserido tratamento de login, logout e configuracoes

--- accounts/views.py
+++ accounts/views.py
@@ -1,26 +1,34 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
 
 def register_view(request):
     if request.method == 'POST':
         user_form = UserCreationForm(request.POST)
         if user_form.is_valid():
             user_form.save()            
-            return redirect('cars_list')  
+            return redirect('login')  
     else:        
         user_form = UserCreationForm()
     return render(request, 'register.html', {'user_form': user_form})
 
 def login_view(request):
     if request.method == 'POST':
+        login_form = AuthenticationForm(data=request.POST)
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('cars_list')  # Redirect to the cars list after login
-        else:
-            error_message = "Invalid credentials"
-            return render(request, 'login.html', {'error_message': error_message})
-    return render(request, 'login.html')
-    
+            return redirect('cars_list')
+        # Se falhar, login_form já contém os erros
+    else:
+        login_form = AuthenticationForm()
+    return render(request, 'login.html', {'login_form': login_form})
+
+def logout_view(request):
+    from django.contrib.auth import logout
+    logout(request)
+    return redirect('login')  # Redirect to login page after logout
+
+
--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from accounts.views import register_view, login_view
+from accounts.views import logout_view, register_view, login_view
 from app import settings
 from django.conf.urls.static import static
 from cars.views import cars_view, new_car_view
@@ -9,6 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register_view, name='register'),  # Assuming you have a registration view
     path('login/', login_view, name='login'),  # Using Django's built-in login view
+    path('logout/', logout_view, name='logout'),  # Assuming you have a logout view
     path('cars/', cars_view, name='cars_list'),  # Include the URLs from the cars app
     path('new_car/', new_car_view, name='new_car'),  # Assuming you have a view for adding new cars
     
